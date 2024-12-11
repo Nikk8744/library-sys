967ff36ed43e64cdd112a9304d4c50e0873966ad
Nikk8744 <nikk8744@gmail.com>
2024-12-12 01:07:42 +0530

    get/isbn delete functions

--- src/controller/book.js
+++ src/controller/book.js
@@ -19,8 +19,54 @@ exports.addBooks = async(req, res) => {
 };
 
 exports.getBooks = async (req, res) => {
-    res.send("Hello Broo")
+
+    try {
+        const books = await db('books').select("*");
+        if (!books) {
+            res.status(401).json({msg: "Something went wrong while fetching books!!"})
+        }
+        res.status(200).json({
+            books,
+            msg: "Books fetched successfully"
+        });
+    } catch (error) {
+        console.log(error)
+    }
 };
+exports.getBooksByIsbn = async (req, res) => {
+    const { isbn } = req.params;
+    if (!isbn) {
+        return res.status(400).json({msg: "Enter the isbn bumber"})
+    }    
+
+    const book = await db('books').where({isbn}).select("*");
+
+    return res.status(200).json({
+        book,
+        msg: "Book fetched successfully"
+    })
+}
+
+exports.editBook = async (req, res) => {
+    // take the isbn number from params as its unique
+    const { isbn } = req.params;
+}
+
+exports.deleteBook = async (req, res) => {
+    const { isbn } = req.params;
+
+    const book = await db('books').where({isbn}).select("*");
+    if (book.length === 0) {
+        return res.status(404).json({msg: "Np such book found!!"})
+    }
+    
+    const deletedBook = await db('books').where({isbn}).del();
+    if (!deletedBook) {
+        return res.status(401).json({msg: "Something went wrong while deleting book!!"})
+    } 
+
+    return res.status(200).json({ msg: "Book deleted"})
+}
 
 // module.exports = {
 //     addBooks,
--- src/routes/books.js
+++ src/routes/books.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
-const { getBooks, addBooks } = require("../controller/book");
+const { getBooks, addBooks, getBooksByIsbn, deleteBook } = require("../controller/book");
 
 const router = Router();
 
 router.get("/get", getBooks);
-
+router.get("/get/:isbn", getBooksByIsbn)
 router.post("/", addBooks);
+
+router.delete("/delete/:isbn", deleteBook);
 
 module.exports = router;  
