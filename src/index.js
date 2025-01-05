9eff119f213a7d35dd256e91049d49c91ca66e4f
Nikk8744 <nikk8744@gmail.com>
2025-01-05 20:56:21 +0530

    swagger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const bookRoutes = require('./routes/books')
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
+
 app.use(express.json());
 
 app.get('/hi', (req, res) => {
@@ -10,6 +14,7 @@ app.get('/hi', (req, res) => {
 })
 
 app.use(bookRoutes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server Running on Port: ${process.env.PORT}`);
